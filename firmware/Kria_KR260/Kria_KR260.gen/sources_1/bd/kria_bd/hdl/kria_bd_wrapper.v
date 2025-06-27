180b27e74346948ed4e846417f6e4fa2c34f34fa
juan <juanvalen15@gmail.com>
2025-06-27 12:54:49 +0200

    created platform in vivado 2022.1 to prevent petalinux 2022.1 to fail with ubuntu 22.04: petalinux part still pending

---
+++ firmware/Kria_KR260/Kria_KR260.gen/sources_1/bd/kria_bd/hdl/kria_bd_wrapper.v
@@ -0,0 +1,18 @@
+//Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2022.1 (lin64) Build 3526262 Mon Apr 18 15:47:01 MDT 2022
+//Date        : Fri Jun 27 12:40:57 2025
+//Host        : jubu running 64-bit Ubuntu 22.04.5 LTS
+//Command     : generate_target kria_bd_wrapper.bd
+//Design      : kria_bd_wrapper
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+module kria_bd_wrapper
+   ();
+
+
+  kria_bd kria_bd_i
+       ();
+endmodule
---
+++ firmware/Kria_KR260/Kria_KR260.runs/synth_1/.Xil/kria_bd_wrapper_propImpl.xdc
@@ -0,0 +1,4 @@
+set_property SRC_FILE_INFO {cfile:/home/juan/Desktop/ws/portfolio/vision-based-motion-control/firmware/Kria_KR260/Kria_KR260.gen/sources_1/bd/kria_bd/ip/kria_bd_clk_wiz_0_0/kria_bd_clk_wiz_0_0.xdc rfile:../../../Kria_KR260.gen/sources_1/bd/kria_bd/ip/kria_bd_clk_wiz_0_0/kria_bd_clk_wiz_0_0.xdc id:1 order:EARLY scoped_inst:kria_bd_i/clk_wiz_0/inst} [current_design]
+current_instance kria_bd_i/clk_wiz_0/inst
+set_property src_info {type:SCOPED_XDC file:1 line:57 export:INPUT save:INPUT read:READ} [current_design]
+set_input_jitter [get_clocks -of_objects [get_ports clk_in1]] 0.100
